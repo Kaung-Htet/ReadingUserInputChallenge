e8dc0e676b62c3b9d46b98ec99edac032963115b
Kaung-Htet <kaunghtetnyilay@gmail.com>
2020-07-13 19:24:44 +0700

    Add coding

--- src/ReadingUserInputChallenge.java
+++ src/ReadingUserInputChallenge.java
@@ -4,24 +4,30 @@ public class ReadingUserInputChallenge {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        //check if user input valid number or not
-        boolean hasNextInt = scanner.hasNextInt();
-
         //to count number
         int count = 1;
 
         //to sum user input numbers
         int sum = 0;
 
-        if (hasNextInt){
-            while (count <= 10){
-                System.out.println("Enter number #" + count);
+        while (count <= 10) {
+
+            System.out.println("Enter number #" + count);
+
+            //check if user input valid number or not
+            boolean hasNextInt = scanner.hasNextInt();
+
+            if (hasNextInt) {
                 int userInput = scanner.nextInt();
                 sum += userInput;
                 count++;
+            } else {
+                System.out.println("Invalid number");
             }
-        } else {
-            System.out.println("Invalid number");
+
+            scanner.nextLine();
         }
+        scanner.close();
+        System.out.println("Total sum numbers = " + sum);
     }
 }
